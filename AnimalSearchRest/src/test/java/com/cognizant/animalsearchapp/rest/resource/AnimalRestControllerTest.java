ee081bae36f6a732d83546272133d74aab751668
Syamala Myneni <syamam1009@gmail.com>
2017-02-20 06:40:59 -0600

    Updated test Cases for RestController

--- AnimalSearchRest/src/test/java/com/cognizant/animalsearchapp/rest/resource/AnimalRestControllerTest.java
+++ AnimalSearchRest/src/test/java/com/cognizant/animalsearchapp/rest/resource/AnimalRestControllerTest.java
@@ -5,6 +5,8 @@ package com.cognizant.animalsearchapp.rest.resource;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -71,10 +73,13 @@ public class AnimalRestControllerTest {
 		List<Animal> animalList = new ArrayList<Animal>();
 		Animal animal = new Animal();
 		animal.setName("tiger");
+		animal.setRegion("USA");
 		Animal animal1 = new Animal();
 		animal1.setName("lion");
+		animal1.setRegion("AFRICA");
 		Animal animal2 = new Animal();
 		animal2.setName("elephant");
+		animal2.setRegion("INDIA");
 		animalList.add(animal);
 		animalList.add(animal1);
 		animalList.add(animal2);
@@ -89,27 +94,32 @@ public class AnimalRestControllerTest {
 				.andExpect(status().isOk()).andReturn();
 		String content = result.getResponse().getContentAsString();
 
+		verify(validator, times(1)).validateXMLSchema(Matchers.anyString(), Matchers.anyString());
+		verify(animalService, times(1)).getAccessLog(Matchers.anyListOf(String.class));
+		verify(animalService, times(1)).logAccessRequest(Matchers.anyListOf(String.class));
+		verify(animalService, times(1)).getAnimalRegionByName(Matchers.anyListOf(String.class));
+
 		assertNotNull(content);
 		assertTrue(content.length() > 0);
+		assertTrue(content.contains("<name>tiger</name>"));
+		assertTrue(content.contains("<region>USA</region>"));
+		assertTrue(content.contains("<name>lion</name>"));
+		assertTrue(content.contains("<region>AFRICA</region>"));
+		assertTrue(content.contains("<name>elephant</name>"));
+		assertTrue(content.contains("<region>INDIA</region>"));
 	}
 
 	@Test
 	public final void testGetAnimalRegionWithBadRequestException() throws Exception {
 
-		List<AnimalAccessLog> logs = new ArrayList<AnimalAccessLog>();
-		AnimalAccessLog accessLog = new AnimalAccessLog();
-		accessLog.setAccessTime(new Date());
-		accessLog.setName("lion,tiger,elephant");
-		accessLog.setRequestId(1);
-		logs.add(accessLog);
-
 		String input = "<animalrequest xmlns=\"com.cognizant.animalearchapp.rest\"><name>Elephant</name><name>tiger</name><name>DOg</name></animalrequest>";
 		Mockito.when(validator.validateXMLSchema(Matchers.anyString(), Matchers.anyString())).thenReturn(false);
-		Mockito.when(animalService.getAccessLog(Matchers.anyListOf(String.class))).thenReturn(logs);
-		Mockito.when(animalService.logAccessRequest(Matchers.anyListOf(String.class))).thenReturn(1);
 
 		MvcResult result = this.mockMvc.perform(get(new URI("/v1/animals/region")).param("requestXml", input))
 				.andExpect(status().isBadRequest()).andReturn();
+
+		verify(validator, times(1)).validateXMLSchema(Matchers.anyString(), Matchers.anyString());
+
 		String content = result.getResponse().getContentAsString();
 
 		assertNotNull(content);
@@ -130,10 +140,13 @@ public class AnimalRestControllerTest {
 		String input = "<animalrequest xmlns=\"com.cognizant.animalearchapp.rest\"><name>Elephant</name><name>tiger</name><name>DOg</name></animalrequest>";
 		Mockito.when(validator.validateXMLSchema(Matchers.anyString(), Matchers.anyString())).thenReturn(true);
 		Mockito.when(animalService.getAccessLog(Matchers.anyListOf(String.class))).thenReturn(logs);
-		Mockito.when(animalService.logAccessRequest(Matchers.anyListOf(String.class))).thenReturn(1);
 
 		MvcResult result = this.mockMvc.perform(get(new URI("/v1/animals/region")).param("requestXml", input))
 				.andExpect(status().isConflict()).andReturn();
+
+		verify(validator, times(1)).validateXMLSchema(Matchers.anyString(), Matchers.anyString());
+		verify(animalService, times(1)).getAccessLog(Matchers.anyListOf(String.class));
+
 		String content = result.getResponse().getContentAsString();
 
 		assertNotNull(content);
